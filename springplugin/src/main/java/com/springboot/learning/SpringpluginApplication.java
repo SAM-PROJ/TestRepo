cef72bc475a38b8380fe1c2ccfc39020292a54f0
SASWATA MALLA <SASWATA MALLA@DESKTOP-G17JGC3>
2024-05-03 23:31:59 +0530

    commit by SAM

---
+++ springplugin/src/main/java/com/springboot/learning/SpringpluginApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpluginApplication.class, args);
+	}
+
+}
